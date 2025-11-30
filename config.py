867ecebcd545d3eb4eb763888da06b610f7c491c
widhvans <110589162+widhvans@users.noreply.github.com>
2025-11-30 13:43:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
 # Telegram Bot Father se Token lein
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8368477494:AAEOSpffeyBsAPkru_0O-NW25MPDvlY_wRY"
 
 # MongoDB Connection String (MongoDB Atlas se lein)
-MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Apna Telegram User ID yaha daalein (Optional: Admin control ke liye)
-OWNER_ID = 123456789
+OWNER_ID = 8303569508
